3c16252b371ed290980552abad0cf389f40f77cf
Seth A Burleson <Sbrevolution5@aim.com>
2025-02-04 15:24:45 -0500

    day3 part1 is complete

--- src/main/java/sb/adventofcode/twentyFour/Day3FileDecorruptor.java
+++ src/main/java/sb/adventofcode/twentyFour/Day3FileDecorruptor.java
@@ -4,29 +4,48 @@ import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
+import static java.lang.Integer.parseInt;
+import static sb.adventofcode.twentyFour.Inputs24.Day3Input;
 import static sb.adventofcode.twentyFour.Inputs24.Day3Sample;
 
 public class Day3FileDecorruptor {
-    public void JustMultiply(){
+    public void JustMultiply() {
         //look for mul
         ArrayList<String> muls = new ArrayList<>();
         extractToList(muls);
         ArrayList<Integer> productList = new ArrayList<>();
-        for(String mul:muls){
-
+        for (String mul : muls) {
+            productList.add(getProductFromString(mul));
         }
-        //does next mul have digits in 1-3 * 1-3) pattern?
-        //put in a row
-        //sum them up
+        int prod = getSum(productList);
+        System.out.printf("%s is sum of dividends", prod);
+    }
+
+    private static int getProductFromString(String mul) {
+        Pattern p = Pattern.compile("\\d{1,3}");
+        Matcher m = p.matcher(mul);
+        ArrayList<Integer> multiples = new ArrayList<>();
+        while (m.find()) {
+            multiples.add(parseInt(m.group()));
+        }
+        return multiples.get(0) * multiples.get(1);
     }
 
     private static void extractToList(ArrayList<String> muls) {
         String pattern = "mul[(]\\d{1,3}[,]\\d{1,3}[)]";
         Pattern p = Pattern.compile(pattern);
-        Matcher m = p.matcher(Day3Sample);
+        Matcher m = p.matcher(Day3Input);
         while (m.find()) {
             System.out.println(m.group());
             muls.add(m.group());
         }
+    }
+    private int getSum(ArrayList<Integer> distances) {
+        var sum = 0;
+        for (Integer dist :
+                distances) {
+            sum +=dist;
+        }
+        return sum;
     }
 }
