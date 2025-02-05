9e155739e7bfb7e799d8b76433ec7f57068793dc
Seth A Burleson <Sbrevolution5@aim.com>
2025-02-05 10:59:35 -0500

    broke part1 because the index for the word is now wrong.

--- src/main/java/sb/adventofcode/twentyFour/Day4WordSearcher.java
+++ src/main/java/sb/adventofcode/twentyFour/Day4WordSearcher.java
@@ -1,10 +1,10 @@
 package sb.adventofcode.twentyFour;
 
 public class Day4WordSearcher {
-    public int HowManyWords(String wordSearchString){
+    public int HowManyWords(String wordSearchString) {
         char[][] wordSearchArray = parseWordSearch(wordSearchString);
         String word = "XMAS";
-        var finds = FindWords(wordSearchArray,word);
+        var finds = FindWords(wordSearchArray, word);
         return finds;
     }
 
@@ -13,8 +13,8 @@ public class Day4WordSearcher {
         for (int i = 0; i < wordSearchArray.length; i++) {
             for (int j = 0; j < wordSearchArray[i].length; j++) {
                 //check if first letter.
-                if (wordSearchArray[i][j]==word.charAt(0)){
-                    wordCount+= searchAll(word,wordSearchArray, i , j);
+                if (wordSearchArray[i][j] == word.charAt(0)) {
+                    wordCount += searchAll(word, wordSearchArray, i, j);
                 }
                 //look in each direction
                 //if second letter found, look ONLY IN THAT DIRECTION
@@ -25,41 +25,41 @@ public class Day4WordSearcher {
 
     private int searchAll(String word, char[][] wordSearchArray, int i, int j) {
 //        needs the plus one to account for 0 index
-        boolean spaceUp = i+1>=word.length();
-        boolean spaceDown = wordSearchArray.length-i>=word.length();
-        boolean spaceLeft = j+1>=word.length();
-        boolean spaceRight = wordSearchArray[0].length-j>=word.length();
+        boolean spaceUp = i + 1 >= word.length();
+        boolean spaceDown = wordSearchArray.length - i >= word.length();
+        boolean spaceLeft = j + 1 >= word.length();
+        boolean spaceRight = wordSearchArray[0].length - j >= word.length();
         var count = 0;
-        if(spaceUp){
-            count+=searchUp(word,wordSearchArray, i , j);
+        if (spaceUp) {
+            count += searchUp(word, wordSearchArray, i, j);
         }
-        if(spaceUp&&spaceLeft){
-            count+=searchUpLeft(word,wordSearchArray, i , j);
+        if (spaceUp && spaceLeft) {
+            count += searchUpLeft(word, wordSearchArray, i, j);
         }
-        if(spaceUp && spaceRight){
-            count+=searchUpRight(word,wordSearchArray, i , j);
+        if (spaceUp && spaceRight) {
+            count += searchUpRight(word, wordSearchArray, i, j);
         }
-        if(spaceDown){
-            count+=searchDown(word,wordSearchArray, i , j);
+        if (spaceDown) {
+            count += searchDown(word, wordSearchArray, i, j);
         }
-        if(spaceDown&&spaceLeft){
-            count+=searchDownLeft(word,wordSearchArray, i , j);
+        if (spaceDown && spaceLeft) {
+            count += searchDownLeft(word, wordSearchArray, i, j);
         }
-        if(spaceDown && spaceRight){
-            count+=searchDownRight(word,wordSearchArray, i , j);
+        if (spaceDown && spaceRight) {
+            count += searchDownRight(word, wordSearchArray, i, j);
         }
-        if(spaceLeft){
-            count+=searchLeft(word,wordSearchArray, i , j);
+        if (spaceLeft) {
+            count += searchLeft(word, wordSearchArray, i, j);
         }
-        if(spaceRight){
-            count+=searchRight(word,wordSearchArray, i , j);
+        if (spaceRight) {
+            count += searchRight(word, wordSearchArray, i, j);
         }
         return count;
     }
 
     private int searchRight(String word, char[][] wordSearchArray, int i, int j) {
         for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i][j+k]!=word.charAt(k)){
+            if (wordSearchArray[i][j + k] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -68,7 +68,7 @@ public class Day4WordSearcher {
 
     private int searchLeft(String word, char[][] wordSearchArray, int i, int j) {
         for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i][j-k]!=word.charAt(k)){
+            if (wordSearchArray[i][j - k] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -76,8 +76,8 @@ public class Day4WordSearcher {
     }
 
     private int searchDownRight(String word, char[][] wordSearchArray, int i, int j) {
-        for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i+k][j+k]!=word.charAt(k)){
+        for (int k = 0; k < word.length(); k++) {
+            if (wordSearchArray[i + k][j + k] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -85,8 +85,8 @@ public class Day4WordSearcher {
     }
 
     private int searchDownLeft(String word, char[][] wordSearchArray, int i, int j) {
-        for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i+k][j-k]!=word.charAt(k)){
+        for (int k = 0; k < word.length(); k++) {
+            if (wordSearchArray[i + k][j - k] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -95,7 +95,7 @@ public class Day4WordSearcher {
 
     private int searchDown(String word, char[][] wordSearchArray, int i, int j) {
         for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i+k][j]!=word.charAt(k)){
+            if (wordSearchArray[i + k][j] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -104,8 +104,8 @@ public class Day4WordSearcher {
 
 
     private int searchUpRight(String word, char[][] wordSearchArray, int i, int j) {
-        for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i-k][j+k]!=word.charAt(k)){
+        for (int k = 0; k < word.length(); k++) {
+            if (wordSearchArray[i - k][j + k] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -113,8 +113,8 @@ public class Day4WordSearcher {
     }
 
     private int searchUpLeft(String word, char[][] wordSearchArray, int i, int j) {
-        for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i-k][j-k]!=word.charAt(k)){
+        for (int k = 0; k < word.length(); k++) {
+            if (wordSearchArray[i - k][j - k] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -124,7 +124,7 @@ public class Day4WordSearcher {
     private int searchUp(String word, char[][] wordSearchArray, int i, int j) {
         //if we don't find second letter return 0, then continue check
         for (int k = 1; k < word.length(); k++) {
-            if (wordSearchArray[i-k][j]!=word.charAt(k)){
+            if (wordSearchArray[i - k][j] != word.charAt(k)) {
                 return 0;
             }
         }
@@ -140,7 +140,8 @@ public class Day4WordSearcher {
         }
         return res;
     }
-    public int HowManyXWords(String wordSearchString){
+
+    public int HowManyXWords(String wordSearchString) {
         //find the A
         //check for a Mas or sam on down diag, then up diag
         //if both are true, add to the count
@@ -156,13 +157,41 @@ public class Day4WordSearcher {
         for (int i = 0; i < wordSearchArray.length; i++) {
             for (int j = 0; j < wordSearchArray[i].length; j++) {
                 //check if first letter.
-                if (wordSearchArray[i][j]=='A'){
-                    wordCount+= searchX(wordSearchArray, i , j);
+                if (wordSearchArray[i][j] == 'A') {
+                    wordCount += searchX(wordSearchArray, i, j);
                 }
                 //look in each direction
                 //if second letter found, look ONLY IN THAT DIRECTION
             }
         }
         return wordCount;
+    }
+
+    private int searchX(char[][] wordSearchArray, int i, int j) {
+        //need a blank character so previous methods start at 2nd letter, they are already looking for "MAS" but if you put in MAS it looks for "AS"
+        var word = " MAS";
+        boolean spaceUp = i > 0;
+        boolean spaceDown = wordSearchArray.length - 1 > i;
+        boolean spaceLeft = j > 0;
+        boolean spaceRight = wordSearchArray[i].length - 1 > j;
+        if(!spaceRight||!spaceLeft||!spaceDown||!spaceUp){
+            return 0;
+        }
+        if (searchUpLeft(word.trim(), wordSearchArray, i + 1, j + 1) == 1) {
+            if (searchUpRight(word.trim(), wordSearchArray, i + 1, j - 1) == 1) {
+                return 1;
+            } else if (searchDownLeft(word.trim(), wordSearchArray, i - 1, j + 1) == 1) {
+                return 1;
+            }
+        } else if (searchDownRight(word.trim(), wordSearchArray, i - 1, j - 1) == 1) {
+            if (searchUpRight(word.trim(), wordSearchArray, i + 1, j - 1) == 1) {
+                return 1;
+            } else if (searchDownLeft(word.trim(), wordSearchArray, i - 1, j + 1) == 1) {
+                return 1;
+            }
+        } else {
+            return 0;
+        }
+        return 0;
     }
 }
